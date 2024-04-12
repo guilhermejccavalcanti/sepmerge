ce357035928ac3c871736134b751acf4939f81b6
Felipe Araujo <fbma@cin.ufpe.br>
2024-04-12 02:04:34 -0300

    chore(SepMerge): Run Diff3 on left, base and right files

--- src/main/java/com/fbmadev/sepmerge/sepmerge_module/SepMerge.java
+++ src/main/java/com/fbmadev/sepmerge/sepmerge_module/SepMerge.java
@@ -1,6 +1,9 @@
 package com.fbmadev.sepmerge.sepmerge_module;
 
+import java.io.IOException;
 import java.nio.file.Path;
+
+import com.fbmadev.sepmerge.diff3_module.Diff3Runner;
 
 public class SepMerge {
 
@@ -9,5 +12,18 @@ public class SepMerge {
     public static void run(Path left, Path base, Path right, Path output) {
         System.err.println("Running SepMerge");
         System.err.println("tmpFolder: " + tmpFolder);
+
+        Path diff3Tmp = runDiff3(left, base, right);
+    }
+
+    private static Path runDiff3(Path left, Path base, Path right) {
+        try {
+            Path diff3Tmp = tmpFolder.resolve("diff3.tmp");
+            Diff3Runner.runDiff3(left.toString(), base.toString(), right.toString(), diff3Tmp.toString());
+            return diff3Tmp;
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
