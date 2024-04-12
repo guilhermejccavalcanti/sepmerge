03dfe8ebf2b10fd353a0d16f4886451646d87212
Felipe Araujo <fbma@cin.ufpe.br>
2024-04-12 01:46:06 -0300

    chore(SepMergeApplication): Add left, base, right and output paths as parameters

--- src/main/java/com/fbmadev/sepmerge/SepmergeApplication.java
+++ src/main/java/com/fbmadev/sepmerge/SepmergeApplication.java
@@ -1,12 +1,29 @@
 package com.fbmadev.sepmerge;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import com.fbmadev.sepmerge.sepmerge_module.SepMerge;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(name = "sepmerge", description = "Merge files files, implementing the 3-way merge algorithm.")
 public class SepmergeApplication implements Runnable {
+
+	@Parameters(index = "0")
+	private String left;
+
+	@Parameters(index = "1")
+	private String base;
+
+	@Parameters(index = "2")
+	private String right;
+
+	@Option(names = { "-o", "--output" }, description = "Output file path.")
+	private String output;
 
 	public static void main(String[] args) {
 		new CommandLine(new SepmergeApplication()).execute(args);
@@ -14,8 +31,12 @@ public class SepmergeApplication implements Runnable {
 
 	@Override
 	public void run() {
-		SepMerge.run(null, null, null, null);
-		// Path currDirr = Paths.get(System.getProperty("user.dir"));
+		Path currDir = Paths.get(System.getProperty("user.dir"));
+		Path leftFile = currDir.resolve(this.left);
+		Path baseFile = currDir.resolve(this.base);
+		Path rightFile = currDir.resolve(this.right);
+		Path outputFile = this.output != null ? currDir.resolve(this.output) : null;
+		SepMerge.run(leftFile, baseFile, rightFile, outputFile);
 	}
 
 }
